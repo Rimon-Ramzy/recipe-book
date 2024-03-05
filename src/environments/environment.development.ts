87243e5012e2f779d30d853362fedb3e5799d4cf
Rimon-Ramzy <rimonramzy023@gmail.com>
2024-03-05 05:35:59 +0200

    Initial Project

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiKey: "AIzaSyAyvR2zjG7R3hYavZV6TcFXXi3dy0Fbu9g"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: "AIzaSyAyvR2zjG7R3hYavZV6TcFXXi3dy0Fbu9g"
+};
